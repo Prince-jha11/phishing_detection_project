117edcdaf7ffbab571dc014129d73b11f23332aa
kaushik2-1 <kaushikrajnish30@gmail.com>
2026-04-02 11:34:48 +0530

    extension origin added

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5500",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "chrome-extension://*"
 ]
 
 app.add_middleware(
